353cf08eb9e05223137e3c51453d8eec058b4492
Will Weaver <monkey@buildingbananas.com>
2015-11-05 20:24:10 +0000

    TA/WW: Fix timing issue

--- musician/main.py
+++ musician/main.py
@@ -4,6 +4,7 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
 while True:
-    GPIO.output(18, True)
-    time.sleep(2)
     GPIO.output(18, False)
+    time.sleep(1)
+    GPIO.output(18, True)
+    time.sleep(1)
